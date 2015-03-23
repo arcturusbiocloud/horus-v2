b59c8432490f6d71ddd0a4443e5dcee1f5138818
luisbebop <luisbebop@gmail.com>
2015-03-22 22:46:08 -0700

    added proper timeout and a way to manipulate the serial handle.

--- horus.go
+++ horus.go
@@ -3,12 +3,25 @@ package main
 import (
   "github.com/go-martini/martini"
   "github.com/codegangsta/martini-contrib/render"
+  "github.com/tarm/serial"
+  "log"
+  "time"
+  "bufio"
 )
 
 func main() {
   m := martini.Classic()
   m.Use(render.Renderer())
-    
+  
+  // init the arduino serial port
+  c := &serial.Config{Name: "/dev/ttyACM0", Baud: 9600, ReadTimeout: time.Millisecond * 2000}
+  s, _ := serial.OpenPort(c)
+  log.Printf("The USB port the Arduino service will use is /dev/ttyACM0")
+  
+  // When connecting to an older revision Arduino, you need to wait
+	// a little while it resets.
+	time.Sleep(1 * time.Second)
+	  
   // execute a script and return the status
   m.Post("/api/project/:script_call", func(r render.Render, params martini.Params) {
     script_call := params["script_call"]
@@ -33,6 +46,32 @@ func main() {
     // test stack hardware
     // ...
     r.JSON(200, map[string]interface{}{"status": "true"})
+  })
+  
+  // check all the machine hardware
+  m.Get("/api/serial/:buffer", func(r render.Render, params martini.Params) {
+    buf := params["buffer"]
+        
+    // check the serial port
+    if s == nil {
+      r.JSON(200, map[string]interface{}{"error": "serial port not connected"})
+      return
+    }
+    
+    // get serial
+    _, err := s.Write([]byte(buf))
+    
+    if err != nil {
+      r.JSON(200, map[string]interface{}{"error": err})
+    } else {
+      bio := bufio.NewReader(s)
+      buf, err := bio.ReadString('\n')
+      if err != nil {
+        r.JSON(200, map[string]interface{}{"error": err})
+      } else {
+        r.JSON(200, map[string]interface{}{"status": string(buf)})
+      }
+    }
   })
     
   m.Run()
