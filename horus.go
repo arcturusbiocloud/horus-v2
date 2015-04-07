442c78d455459b4399962531c9ee1ebe6bc11b11
luisbebop <luisbebop@gmail.com>
2015-04-06 18:21:50 -0700

    fixed variable declaration problem

--- horus.go
+++ horus.go
@@ -151,7 +151,7 @@ func main() {
     proc.Run()
     
     // take picture
-    proc := exec.Command("v4l2-ctl", "-d", "/dev/video1", "-c", "focus_auto=1")
+    proc = exec.Command("v4l2-ctl", "-d", "/dev/video1", "-c", "focus_auto=1")
     proc.Run()
     proc = exec.Command("v4l2-ctl", "--set-fmt-video=width=1920,height=1080,pixelformat=1")
     proc.Run()
