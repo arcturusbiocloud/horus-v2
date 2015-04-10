8fff7f22a1cd80b49bff62f22b3f821aeb4f91af
luisbebop <luisbebop@gmail.com>
2015-04-09 18:27:18 -0700

    Fixed problem with bin folder uploading

--- build_upload.sh
+++ build_upload.sh
@@ -1,5 +1,5 @@
 go clean
 GOARM=7 GOARCH=arm GOOS=linux go build
-#scp -r ~/Documents/arcturusbiocloud/horus-v2/bin root@10.1.10.111:/root/horus-v2/bin
+#scp -r ~/Documents/arcturusbiocloud/horus-v2/bin root@10.1.10.111:/root/horus-v2/
 #scp -r ~/Documents/arcturusbiocloud/horus-v2/streaming root@10.1.10.111:/root/horus-v2/streaming
 scp ~/Documents/arcturusbiocloud/horus-v2/horus-v2 root@10.1.10.111:/root/horus-v2/
