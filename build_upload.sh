f982cf7f936e4e3530cc4c374e317dc320e0f777
luisbebop <luisbebop@gmail.com>
2015-05-30 19:47:47 -0700

    Changed upload endpoint

--- build_upload.sh
+++ build_upload.sh
@@ -2,4 +2,4 @@ go clean
 GOARM=7 GOARCH=arm GOOS=linux go build
 #scp -r ~/Documents/arcturusbiocloud/horus-v2/bin root@10.1.10.111:/root/horus-v2/
 #scp -r ~/Documents/arcturusbiocloud/horus-v2/streaming root@10.1.10.111:/root/horus-v2/streaming
-scp ~/Documents/arcturusbiocloud/horus-v2/horus-v2 root@10.1.10.111:/root/horus-v2/
+scp ~/Documents/arcturusbiocloud/horus-v2/horus-v2 root@192.168.7.42:/root/horus-v2/
