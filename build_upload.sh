22d72dd3f2fad6a79ad8bff66cb017cb399a4865
luisbebop <luisbebop@gmail.com>
2015-06-08 19:04:20 -0700

    Updated upload shell script with new ips and comments

--- build_upload.sh
+++ build_upload.sh
@@ -1,5 +1,9 @@
 go clean
 GOARM=7 GOARCH=arm GOOS=linux go build
-#scp -r ~/Documents/arcturusbiocloud/horus-v2/bin root@10.1.10.111:/root/horus-v2/
-#scp -r ~/Documents/arcturusbiocloud/horus-v2/streaming root@10.1.10.111:/root/horus-v2/streaming
+
+# copy additional files
+#scp -r ~/Documents/arcturusbiocloud/horus-v2/bin root@192.168.7.42:/root/horus-v2/
+#scp -r ~/Documents/arcturusbiocloud/horus-v2/streaming root@192.168.7.42:/root/horus-v2/streaming
+
+# copy just the executable
 scp ~/Documents/arcturusbiocloud/horus-v2/horus-v2 root@192.168.7.42:/root/horus-v2/
