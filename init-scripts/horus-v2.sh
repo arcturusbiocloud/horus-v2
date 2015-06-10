d76e84a27b18a0691a3f0c1c347e1e7a141f3801
luisbebop <luisbebop@gmail.com>
2015-06-10 14:42:36 -0700

    Added init script to rpi2

---
+++ init-scripts/horus-v2.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# /etc/init.d/horus-v2
+
+OUT_LOG=/dev/null
+HORUS_BINARY=/home/pi/horus-v2/horus-v2
+
+case "$1" in
+  start)
+    echo "Starting horus-v2"
+    $HORUS_BINARY 1>$OUT_LOG 2>$OUT_LOG &
+    ;;
+  stop)
+    echo "Stopping horus-v2"
+    killall horus-v2
+    ;;
+  *)
+    echo "Usage: /etc/init.d/horus-v2 {start|stop}"
+    exit 1
+    ;;
+  esac
+exit 0
